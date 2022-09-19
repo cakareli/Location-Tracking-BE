a7b6fc2ba3683294e0dee3ccfb75bfb5a3d77336
cakareli <strainjacvijanovic@gmail.com>
2022-09-19 21:28:08 +0200

    app added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const app = require('express')()
 const http = require("http")
-const server = http.createServer();
+const server = http.createServer(app);
 const {Server} = require("socket.io")
 const io = new Server(server)
 
 const PORT = process.env.PORT || 3700;
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running")
+})
 
  io.on("connection", (socket) => {
      socket.io("position-change", (data) => {
